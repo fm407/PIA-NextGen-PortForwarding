c54e7f9c2f9f378d1126f19842cce5528e96ba65
Francesco <38185544+fm407@users.noreply.github.com>
2020-09-13 09:09:25 -0400

    Create transportupdate.sh

---
+++ transportupdate.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+export PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:/root/bin
+
+# Vers: 1.2
+# Date: 1.2.2020
+# This script has to be placed in the Transmission seedbox
+# Script by HolyK https://forum.netgate.com/user/holyk
+
+############ Update these please #############
+
+# Transmission-remote WEB credentials
+USERNAME='TRANSMISSION WEBUI USER'
+PASSWORD='TRANSMISSION WEBUI PASS'
+
+# Transmission-remote binary is usually under known environment location.
+# Validate the command is known by " which transmission-remote "
+# If the "transmission-remote" is not known try to " find / -name transmission-remote "
+# Then update the variable bellow with full-path to the binary
+#TRANSREMOTE='transmission-remote'
+TRANSREMOTE='/usr/bin/transmission-remote'
+
+############ Rest of the code - do not touch #############
+
+# Port numbers
+NEWPORT="$1"
+
+# Verify that received new port is a valid number.
+if ! [ "$NEWPORT" -eq "$NEWPORT" ] 2> /dev/null; then
+    logger "Non-numeric port ( $NEWPORT ) received from remote host. Aborting!"
+    # EMAIL
+    exit 1
+fi
+
+# Check if Transmission is running
+service transmission-daemon status
+TRANSSVCRC=$?
+if [ "$TRANSSVCRC" -gt 0  ]; then
+  logger "Transmission service is not running. Port update aborted!"
+  # EMAIL
+        exit 1
+else
+  # Configure new port received from remote system
+  $TRANSREMOTE --auth ${USERNAME}:${PASSWORD} -p ${NEWPORT}
+  TRANSREMOTERC=$?
+  if [ "$TRANSREMOTERC" -gt 0  ]; then
+    logger "Error when calling transmission-remote binary. Port was NOT updated!"
+    # EMAIL
+         exit 1
+  fi
+  logger "Transmission port succesfully updated. New port is: ${NEWPORT}"
+  exit 0
+fi
